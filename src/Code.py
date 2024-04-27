1d5fd4d85ecbf3469b6538b8c5553c96fb6f69e7
edward.baleni <baleniedward@gmail.com>
2024-04-27 16:23:39 +0200

    Masking function complete

--- src/Code.py
+++ src/Code.py
@@ -2,5 +2,8 @@
 # If that is not available then I want to 
 # call upon the data file to create npy file, and open data here
 
+
+# https://stackoverflow.com/questions/48376580/how-to-read-data-in-google-colab-from-my-google-drive
+# 
 from google.colab import drive
 drive.mount('/content/drive')
--- src/data.py
+++ src/data.py
@@ -11,6 +11,8 @@
 # %%
 import dataCollect # contains os, glob, random
 
+import shapely # Used for mask creation
+
 import geopandas as gpd
 import rasterio as rio
 from rasterio.plot import show
@@ -53,14 +55,14 @@ Blues = []
     # raster.descriptions
     # range is the sample size
 for i in range(sampleSize):
-    NIRs.append(rio.open([j for j in data_paths_tif[0] if "nir_native" in j][0]).read())
-    Reds.append(rio.open([j for j in data_paths_tif[0] if "red_native" in j][0]))
-    Regs.append(rio.open([j for j in data_paths_tif[0] if "reg_native" in j][0]))
-    RGBs.append(rio.open([j for j in data_paths_tif[0] if "visible_5cm" in j][0], "w"))
+    NIRs.append(rio.open([j for j in data_paths_tif[i] if "nir_native" in j][0]))
+    Reds.append(rio.open([j for j in data_paths_tif[i] if "red_native" in j][0]))
+    Regs.append(rio.open([j for j in data_paths_tif[i] if "reg_native" in j][0]))
+    RGBs.append(rio.open([j for j in data_paths_tif[i] if "visible_5cm" in j][0]))
     # Here we read in the DEM and RGB, change the meta data and save
     # new instance to memory, and close original file
      
-    DEMs.append(rio.open([j for j in data_paths_tif[0] if "dem_native" in j][0]))
+    DEMs.append(rio.open([j for j in data_paths_tif[i] if "dem_native" in j][0]))
     # Red, Green, Blue can be taken from RGB
     # Red is the first, Green is second and Blue is third
     # You can see this when you change around the plotting
@@ -72,7 +74,7 @@ for i in range(sampleSize):
     Blues.append(RGBs[i].read(3))
     Points.append(gpd.GeoDataFrame.from_file(data_paths_geojson[i]))
 
-    # es._stack_bands([Reds[0], NIRs[0]])
+    # es._stack_bands([Reds[0], NIRs[0]]) # to stack bands
 
 # Make function to change transform to same as bands for everything
 
@@ -86,74 +88,84 @@ def getFeatures(gdf):
     import json
     return [json.loads(gdf.to_json())['features'][0]['geometry']]
 
-
 # %%
 
-def 
+def getMaskBounds(comp, shape):
     # https://stackoverflow.com/questions/40385782/make-a-union-of-polygons-in-geopandas-or-shapely-into-a-single-geometry
+    # enclose this in a function
 
-from shapely.ops import unary_union
-from shapely import LineString, Point, Polygon, BufferCapStyle, BufferJoinStyle
-from shapely import buffer
-from shapely import normalize, Polygon
-import shapely
+        # Obtain example file
+    # a = gpd.GeoDataFrame.from_file(data_paths_geojson[1])
+    # point = list(a.iloc[:,1])
+    point = list(shape.iloc[:,1])
 
+        # Combine all intersecting polygons
+    point = shapely.unary_union(point)
+        # Combine all non-intersecting polygons
+    boundary = shapely.coverage_union_all(point)
+        # Get the convex hull of the entire area (different from concave hull)
+        # concave hull will get all the indents
+        # will get more specific boundary with concave
+        # maybe go with concave
+        # Concave actually doesn't work well at all
+        # However, convex is not perfect.
+        # Concave works when you change the ratio
+    boundary_periph = shapely.concave_hull(boundary, ratio=0.15)#shapely.convex_hull(boundary)
+    #poly = gpd.GeoSeries(boundary_periph, crs=RGBs[0].crs.data)
+    poly = gpd.GeoDataFrame(geometry=gpd.GeoSeries(boundary_periph), crs=comp.crs.data)
 
-# enclose this in a function
+    return (poly)
 
-    # Obtain example file
-a = gpd.GeoDataFrame.from_file(data_paths_geojson[1])
-point = list(a.iloc[:,1])
+# Masks data and gives all data the same transform
+def getMask(rast, shape, placeHolder, out_tif="C:\\Users\\balen\\OneDrive\\Desktop\\Git\\Dissertation-AnomalyDetection\\Dissertation-AnomalyDetection\\src\\out.tif"):
+    # Obtain masking, in terms of NIR file
+    poly = getMaskBounds(comp=placeHolder, shape=shape)
+    # Get features for masking
+    coords = getFeatures(poly)
+    # Get information to output masking
+    out_img, out_transform = mask(rast, shapes=coords, crop=True, all_touched=True, pad = True)
+    
+    # The meta data has to be the same as 
+    out_meta = placeHolder.meta.copy()
+    # Change transform here
+    out_transform = placeHolder.transform
+    print(out_meta)
+    epsg_code = int(rast.crs.data['init'][5:])
+    print(epsg_code)
 
-    # Combine all intersecting polygons
-point = unary_union(point)
-    # Combine all non-intersecting polygons
-boundary = shapely.coverage_union_all(point)
-    # Get the convex hull of the entire area (different from concave hull)
-    # concave hull will get all the indents
-    # will get more specific boundary with concave
-    # maybe go with concave
-    # Concave actually doesn't work well at all
-    # However, convex is not perfect.
-    # Concave works when you change the ratio
-boundary_periph = shapely.concave_hull(boundary, ratio=0.15)#shapely.convex_hull(boundary)
-#poly = gpd.GeoSeries(boundary_periph, crs=RGBs[0].crs.data)
-poly = gpd.GeoDataFrame(geometry=gpd.GeoSeries(boundary_periph), crs=RGBs[0].crs.data)
+    out_meta.update({"driver": "GTiff",
+                    "count": rast.count,
+                    "height": out_img.shape[1],
+                    "width": out_img.shape[2],
+                    "transform": out_transform,
+                    "crs": epsg_code})
 
-#boundary = gpd.GeoSeries(unary_union(point))
-boundary_periph
-boundary_periph.boundary
+    #out_tif = "C:\\Users\\balen\\OneDrive\\Desktop\\Git\\Dissertation-AnomalyDetection\\Dissertation-AnomalyDetection\\src\\out2.tif"
+    with rio.open(out_tif, "w", **out_meta) as dest:
+        dest.write(out_img)
+
 
 
 # in a try - except, try mask , except create mask, and apply mask. 
+# If we can't find masked file in data2 then mask it and obtain else obtain
+
+
+# At this stage the masking is fine, 
+# The transform is equal over all bands, however, the width and height isn't the same for each
+# Everything except the width and height is good. Now we need to fix this
+# Output all results to data2
+
+
 
 # %%
-def getFeatures(gdf):
-    """Function to parse features from GeoDataFrame in such a manner that rasterio wants them"""
-    import json
-    return [json.loads(gdf.to_json())['features'][0]['geometry']]
-# %%
 
-coords = getFeatures(poly)#shapely.to_geojson(boundary_periph, indent=1)#getFeatures(shapely.to_json(boundary_periph))
-out_img, out_transform = mask(RGBs[1], shapes=coords, crop=True, all_touched=True, pad = True)
-out_meta = RGBs[1].meta.copy()
-print(out_meta)
-epsg_code = int(RGBs[1].crs.data['init'][5:])
-print(epsg_code)
-
-out_meta.update({"driver": "GTiff",
-                 "height": out_img.shape[1],
-                 "width": out_img.shape[2],
-                 "transform": out_transform,
-                 "crs": epsg_code})
-
-out_tif = "C:\\Users\\balen\\OneDrive\\Desktop\\Git\\Dissertation-AnomalyDetection\\Dissertation-AnomalyDetection\\src\\out.tif"
-with rio.open(out_tif, "w", **out_meta) as dest:
-   dest.write(out_img)
+getMask(NIRs[0], Points[0], NIRs[0],out_tif = "C:\\Users\\balen\\OneDrive\\Desktop\\Git\\Dissertation-AnomalyDetection\\Dissertation-AnomalyDetection\\src\\out1.tif")
+getMask(DEMs[0], Points[0], NIRs[0],out_tif = "C:\\Users\\balen\\OneDrive\\Desktop\\Git\\Dissertation-AnomalyDetection\\Dissertation-AnomalyDetection\\src\\out2.tif")
 
 # %%
 clipped = rio.open(out_tif)
 show((clipped), cmap='terrain')
+#es._stack_bands([clipped, clipped1])
 
 # %%
     # We need a conventional way to save the data after reading it in so that 
