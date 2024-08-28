67b5b08860f82b27ba204652f6a5e29c75142edf
edward.baleni <baleniedward@gmail.com>
2024-08-28 17:13:39 +0200

    Tried very many examples, will add this to appendix to help inform decision making

--- src/BaseModels.py
+++ src/BaseModels.py
@@ -250,13 +250,61 @@ tryout.plot.imshow(ax=ax)
 anomaly_1.plot(ax=ax, facecolor='none', edgecolor='blue')
 nominal_1.plot(ax=ax, facecolor='none', edgecolor='red')
 
+# %%
+
+	# Semi-supervised approach
+from pyod.models.copod import COPOD
+from pyod.models.vae import VAE
+from pyod.models.cof import COF
+
+clf = COPOD()
+X = np.array(data.loc[:, "confidence":])
+clf.fit(X)
+outlier_scores = clf.labels_
+outlierness = clf.decision_scores_
+
+anomaly_1 = data[outlier_scores == 1]
+nominal_1 = data[outlier_scores == 0]
+
+fig, ax = plt.subplots(figsize=(15, 15))
+tryout.plot.imshow(ax=ax)
+anomaly_1.plot(ax=ax, facecolor='none', edgecolor='blue')
+nominal_1.plot(ax=ax, facecolor='none', edgecolor='red')
+
+# TODO: try a combination of semi-supervised approaches.
+#		Reconstruction does not work. But neighbourhood approach does
+#		Worth trying a couple
+# 		Problem with this method is that it takes everything as not an outlier
+clf = COF()
+
+clf.fit(np.array(nominal_1.loc[:, "confidence":]))
+outlier_scores = clf.labels_
+outlierness = clf.decision_scores_
+
+# %%
+# get the prediction on the test data
+y_test_pred = clf.predict(anomaly_1.loc[:, "confidence":])  # outlier labels (0 or 1)
+#y_test_scores = clf.decision_function(anomaly_1.loc[:, "confidence":])  # outlier scores
+
+anomaly_2 = anomaly_1[y_test_pred == 1]
+nominal_2 = anomaly_1[y_test_pred == 0]
+
+fig, ax = plt.subplots(figsize=(15, 15))
+tryout.plot.imshow(ax=ax)
+anomaly_2.plot(ax=ax, facecolor='none', edgecolor='red')
+nominal_1.plot(ax=ax, facecolor='none', edgecolor='white')
+nominal_2.plot(ax=ax, facecolor='none', edgecolor='blue')
+
+
+
+
+
 # %% 
 # can run many at once
 
-
-
-
 # https://github.com/yzhao062/pyod/blob/master/examples/compare_all_models.py
+# https://arxiv.org/pdf/2206.09426
+# https://pyod.readthedocs.io/en/latest/index.html
 
 # Import all models
 from pyod.models.abod import ABOD
@@ -293,17 +341,49 @@ detector_list = [LOF(n_neighbors=5), LOF(n_neighbors=10), LOF(n_neighbors=15),
 				 LOF(n_neighbors=35), LOF(n_neighbors=40), LOF(n_neighbors=45),
 				 LOF(n_neighbors=50)]
 
-# Show the statics of the data
-print('Number of inliers: %i' % n_inliers)
-print('Number of outliers: %i' % n_outliers)
-print(
-	'Ground truth shape is {shape}. Outlier are 1 and inliers are 0.\n'.format(
-		shape=ground_truth.shape))
-print(ground_truth, '\n')
-
 
 # %%
 random_state = 42
+
+# Define the number of inliers and outliers
+n_samples = 200
+outliers_fraction = 0.1
+clusters_separation = [0]
+
+from numpy import percentile
+import matplotlib.font_manager
+import matplotlib.ticker as ticker
+
+import umap
+
+df_dr = data.loc[:, "confidence":]
+#embedding = umap.UMAP(n_neighbors=5).fit_transform(np.array(df_dr))#X)
+embedding = pd.DataFrame(umap.UMAP(n_neighbors=5).fit_transform(np.array(df_dr)))
+
+# TODO: Lower dimensionality already achieved by UMAP, now we shou
+# Plot points in 2D so we can use dimensionality reduction to do this
+# 
+random_state = 42
+
+# Define the number of inliers and outliers
+n_samples = 200
+outliers_fraction = 0.1
+clusters_separation = [0]
+
+# Compare given detectors under given settings
+# Initialize the data
+import math
+x_min = math.floor(min(embedding.iloc[:,0]))
+x_max = round(max(embedding.iloc[:,0]))
+y_min = math.floor(min(embedding.iloc[:,1]))
+y_max = round(max(embedding.iloc[:,1]))
+
+xx, yy = np.meshgrid(np.linspace(-1, x_max, 100), np.linspace(-1, y_max, 100))
+n_inliers = int((1. - outliers_fraction) * n_samples)
+n_outliers = int(outliers_fraction * n_samples)
+ground_truth = np.zeros(n_samples, dtype=int)
+ground_truth[-n_outliers:] = 1
+
 # Define nine outlier detection tools to be compared
 classifiers = {
 	'Angle-based Outlier Detector (ABOD)':
@@ -357,6 +437,7 @@ classifiers = {
 
 	'ECDF-baseD Outlier Detection (ECOD)': ECOD(
 		contamination=outliers_fraction),
+		
 	'Kernel Density Functions (KDE)': KDE(contamination=outliers_fraction),
 
 	'QMCD': QMCD(
@@ -367,9 +448,9 @@ classifiers = {
 
 	'LUNAR': LUNAR(),
 
-	'Cluster-based Local Outlier Factor (CBLOF)':
-		CBLOF(contamination=outliers_fraction,
-			  check_estimator=False, random_state=random_state),
+	'Variational Autoencoder (VAE)':
+		VAE(contamination=outliers_fraction,
+			  random_state=random_state),
 
 	'One-class SVM (OCSVM)': OCSVM(contamination=outliers_fraction),
 }
@@ -414,80 +495,65 @@ plt.show()
 
 
 
-# %%
-# TODO: Need to do this to plot in 2 dimensions
-#       Z should be a mesh with the same number of dimensions as X's length
-from numpy import percentile
-import matplotlib.font_manager
-import matplotlib.ticker as ticker
-
-# Plot points in 2D so we can use dimensionality reduction to do this
-# 
-# Define the number of inliers and outliers
-n_samples = 200
-outliers_fraction = 0.25
-clusters_separation = [0]
-
-# Compare given detectors under given settings
-# Initialize the data
-xx, yy = np.meshgrid(np.linspace(-7, 7, 100), np.linspace(-7, 7, 100))
-n_inliers = int((1. - outliers_fraction) * n_samples)
-n_outliers = int(outliers_fraction * n_samples)
-ground_truth = np.zeros(n_samples, dtype=int)
-ground_truth[-n_outliers:] = 1
-
 # Fit the models with the generated data and
 # compare model performances
+
 for i, offset in enumerate(clusters_separation):
 	np.random.seed(42)
-	# Data generation
-	X1 = 0.3 * np.random.randn(n_inliers // 2, 2) - offset
-	X2 = 0.3 * np.random.randn(n_inliers // 2, 2) + offset
-	X = np.r_[X1, X2]
-	# Add outliers
-	X = np.r_[X, np.random.uniform(low=-6, high=6, size=(n_outliers, 2))]
-
-	# Fit the model
-	plt.figure(figsize=(20, 22))
+	X = np.array(data.loc[:, "confidence":])  
+	plt.figure(figsize=(22, 22)) # Fit the model
 	for i, (clf_name, clf) in enumerate(classifiers.items()):
 		print()
 		print(i + 1, 'fitting', clf_name)
 		# fit the data and tag outliers
+		X = np.array(embedding)
 		clf.fit(X)
 		scores_pred = clf.decision_function(X) * -1
 		y_pred = clf.predict(X)
 		threshold = percentile(scores_pred, 100 * outliers_fraction)
-		n_errors = (y_pred != ground_truth).sum()
+		
 		# plot the levels lines and the points
-
+		anomaly_1 = X[outlier_scores == 0]
+		nominal_1 = X[outlier_scores == 1]
+		
 		Z = clf.decision_function(np.c_[xx.ravel(), yy.ravel()]) * -1
 		Z = Z.reshape(xx.shape)
+		print([threshold, Z.max()])
+		n_outliers = (outlier_scores == 1)
 		subplot = plt.subplot(5, 5, i + 1)
 		subplot.contourf(xx, yy, Z, levels=np.linspace(Z.min(), threshold, 7),
 						 cmap=plt.cm.Blues_r)
 		# a = subplot.contour(xx, yy, Z, levels=[threshold],
 		#                     linewidths=2, colors='red')
-		subplot.contourf(xx, yy, Z, levels=[threshold, Z.max()],
+		subplot.contourf(xx, yy, Z, levels=sorted([threshold, Z.max()]),
 						 colors='orange')
-		b = subplot.scatter(X[:-n_outliers, 0], X[:-n_outliers, 1], c='white',
-							s=20, edgecolor='k')
-		c = subplot.scatter(X[-n_outliers:, 0], X[-n_outliers:, 1], c='black',
-							s=20, edgecolor='k')
+		b = subplot.scatter(anomaly_1[:,0], anomaly_1[:,1], c='white',
+		 					s=20, edgecolor='k')
+		c = subplot.scatter(nominal_1[:,0], nominal_1[:,1], c='black',
+		 					s=20, edgecolor='k')
+		#c = subplot.scatter(X[:, 0], X[:, 1], c='black',s=20, edgecolor='k')
 		subplot.axis('tight')
 		subplot.legend(
 			[
 				# a.collections[0],
-				b, c],
+				 b, c],
 			[
 				# 'learned decision function',
 				'true inliers', 'true outliers'],
 			prop=matplotlib.font_manager.FontProperties(size=10),
 			loc='lower right')
-		subplot.set_xlabel("%d. %s (errors: %d)" % (i + 1, clf_name, n_errors))
-		subplot.set_xlim((-7, 7))
-		subplot.set_ylim((-7, 7))
+		subplot.set_xlabel("%d. %s" % (i + 1, clf_name))
+		subplot.set_xlim((-1, x_max))
+		subplot.set_ylim((-1, y_max))
 	plt.subplots_adjust(0.04, 0.1, 0.96, 0.94, 0.1, 0.26)
 	plt.suptitle("25 outlier detection algorithms on synthetic data",
 				 fontsize=35)
 	
 plt.show()
+
+
+
+# %% 
+
+# TODO: Ensemble
+# 		https://pyod.readthedocs.io/en/latest/pyod.models.html#pyod.models.combination.majority_vote
