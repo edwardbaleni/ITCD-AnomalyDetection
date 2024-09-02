59cebcd57733d59ef687e5258af563ebad2e5351
edward.baleni <baleniedward@gmail.com>
2024-09-02 09:21:00 +0200

    Clean out GoogleAPI stuff, unnecessary

--- src/dataHandler/Trial-GoogleAPI/APICode.py
+++
@@ -1,9 +0,0 @@
-# Essentially, I want to look for an npy file
-# If that is not available then I want to 
-# call upon the data file to create npy file, and open data here
-
-
-# https://stackoverflow.com/questions/48376580/how-to-read-data-in-google-colab-from-my-google-drive
-# 
-from google.colab import drive
-drive.mount('/content/drive')
