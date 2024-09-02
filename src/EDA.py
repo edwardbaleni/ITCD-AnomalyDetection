e794e7293601d9c4eecf74e4c8bcfb6c46aa274d
edward.baleni <baleniedward@gmail.com>
2024-09-02 11:02:27 +0200

    Implemented PFA, an unsupervised feature selection approach. Will try others

--- src/EDA.py
+++ src/EDA.py
@@ -69,7 +69,56 @@ X.drop(concol, axis = 1)
 
 
 # %%
+# https://doi.org/10.1016/j.jocs.2021.101502
+# https://stats.stackexchange.com/questions/108743/methods-in-r-or-python-to-perform-feature-selection-in-unsupervised-learning
+# Gives exact oppositie to VariableThresholding
+from sklearn.decomposition import PCA
+from sklearn.cluster import KMeans
+from collections import defaultdict
+from sklearn.metrics.pairwise import euclidean_distances
+from sklearn.preprocessing import StandardScaler
 
+class PFA(object):
+    def __init__(self, n_features, q=None):
+        self.q = q
+        self.n_features = n_features
+
+    def fit(self, X):
+        if not self.q:
+            self.q = X.shape[1]
+
+        sc = StandardScaler()
+        X = sc.fit_transform(X)
+
+        pca = PCA(n_components=self.q).fit(X)
+        A_q = pca.components_.T
+
+        kmeans = KMeans(n_clusters=self.n_features).fit(A_q)
+        clusters = kmeans.predict(A_q)
+        cluster_centers = kmeans.cluster_centers_
+
+        dists = defaultdict(list)
+        for i, c in enumerate(clusters):
+            dist = euclidean_distances([A_q[i, :]], [cluster_centers[c, :]])[0][0]
+            dists[c].append((i, dist))
+
+        self.indices_ = [sorted(f, key=lambda x: x[1])[0][0] for f in dists.values()]
+        self.features_ = X[:, self.indices_]
+
+
+import numpy as np
+#X = np.random.random((1000,1000))
+X = data.loc[:,"confidence":]
+pfa = PFA(n_features=15)
+pfa.fit(np.array(X))
+
+# To get the transformed matrix
+Y = pfa.features_
+
+# To get the column indices of the kept features
+column_indices = pfa.indices_
+
+print(data.loc[:,"confidence":].iloc[:, column_indices])
 
 
 # %%
